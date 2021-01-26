4ff8d5d26ca0c3fef4ba50a0b6cbdbe60fe0a30a
unknown <hconway@luc.edu>
2021-01-25 19:27:03 -0600

    completed 4? tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,7 +1,7 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
@@ -10,6 +10,26 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testTwo() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch]Code"));
+    }
+
+    @Test
+    public void testThree() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode[]"));
+    }
+
+    @Test
+    public void testFour() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launc[hCode"));
     }
 
 
